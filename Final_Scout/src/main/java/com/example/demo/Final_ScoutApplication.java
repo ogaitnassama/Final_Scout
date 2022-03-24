23760271be9b57ca436fdbdb140b065458ae198c
Ogaitnas <tiagodiaz03@gmail.com>
2022-03-24 14:48:52 +0000

    main file added, may have been deleted by accident, application runs perfectly now

---
+++ Final_Scout/src/main/java/com/example/demo/Final_ScoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Final_ScoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Final_ScoutApplication.class, args);
+	}
+
+}
