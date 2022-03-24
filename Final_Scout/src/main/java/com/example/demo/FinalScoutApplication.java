0cd8b80ae1bdd4460838488b701921ab394693d4
Ogaitnas <tiagodiaz03@gmail.com>
2022-03-24 14:34:20 +0000

    addint the project file

---
+++ Final_Scout/src/main/java/com/example/demo/FinalScoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalScoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalScoutApplication.class, args);
+	}
+
+}
